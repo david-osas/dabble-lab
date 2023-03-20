0a999bfb8e9a1103e8bcb1aa5d0fdc3ec5634c60
osarumense <azamegbeosarumense@gmail.com>
2023-03-20 16:42:35 +0100

    feat: add graphql decorators to country population schema

--- src/country-population/schemas/country-population.schema.ts
+++ src/country-population/schemas/country-population.schema.ts
@@ -1,25 +1,34 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
 
+@ObjectType()
 @Schema({ timestamps: true })
 export class CountryPopulation {
+  @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
 
+  @Field()
   @Prop()
   country: string;
 
+  @Field(() => Int)
   @Prop()
   year: number;
 
+  @Field(() => Int)
   @Prop()
   area: number;
 
+  @Field(() => Int)
   @Prop()
   totalPopulation: number;
 
+  @Field()
   @Prop()
   createdAt: Date;
 
+  @Field()
   @Prop()
   updatedAt: Date;
 }
